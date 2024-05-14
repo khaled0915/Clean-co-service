b5d21c059919da95f60132e529677e713af98c09
khaled0915 <khaledsaifullah50956@gmail.com>
2024-05-15 03:04:58 +0600

    created navbar

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,6 +1,9 @@
 
 import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
+
+import Navbar from './Navbar';
+
+import Sidebar from './sidebar';
 
 const MainLayout = ({children}) => {
     return (
@@ -9,53 +12,18 @@ const MainLayout = ({children}) => {
         <div className="drawer-content flex flex-col">
           {/* Navbar */}
           <div className="w-full navbar bg-base-300">
-            <div className="flex-none lg:hidden">
-              <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-              </label>
-            </div> 
-            <div className="flex-1 px-2 mx-2">Navbar Title</div>
-            <div className="flex-none hidden lg:block">
-              <div className="menu menu-horizontal">
-                {/* Navbar menu content here */}
-                <NavLink to='/about'
-                
-                className={({ isActive }) =>
-                 isActive ? 'btn btn-primary btn-sm' : 
 
-               "btn btn-ghost btn-sm"
-                
-                } > about </NavLink>
+           <Navbar></Navbar>
 
+            
 
-                <NavLink to='/contact' className={({ isActive }) =>
-                 isActive ? 'btn btn-primary btn-sm' : 
-
-               "btn btn-ghost btn-sm"
-                
-                }
-                
-                > contact  </NavLink>
-                <NavLink to='/login' className={({ isActive }) =>
-                 isActive ? 'btn btn-primary btn-sm' : 
-
-               "btn btn-ghost btn-sm"
-                
-                }> Login </NavLink>
-              </div>
-            </div>
           </div>
           {/* Page content here */}
           {children}
         </div> 
         <div className="drawer-side">
           <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label> 
-          <div className="menu menu-horizontal">
-             
-                <NavLink to='/about'> about </NavLink>
-                <NavLink to='/contact'> contact  </NavLink>
-                <NavLink to='/login'> Login </NavLink>
-              </div>
+         <Sidebar></Sidebar>
         </div>
       </div>
     );
---
+++ src/components/layout/sidebar.jsx
@@ -0,0 +1,36 @@
+import { NavLink } from "react-router-dom";
+
+
+const Sidebar = () => {
+    return (
+        <div className="flex flex-col gap-2">
+                {/* Navbar menu content here */}
+                <NavLink to='/about'
+                
+                className={({ isActive }) =>
+                 isActive ? 'btn btn-primary ' : 
+
+               "btn btn-ghost "
+                
+                } > about </NavLink>
+
+
+                <NavLink to='/contact' className={({ isActive }) =>
+                 isActive ? 'btn btn-primary ' : 
+
+               "btn btn-ghost "
+                
+                }
+                
+                > contact  </NavLink>
+                <NavLink to='/login' className={({ isActive }) =>
+                 isActive ? 'btn btn-primary ' : 
+
+               "btn btn-ghost "
+                
+                }> Login </NavLink>
+              </div>
+    );
+};
+
+export default Sidebar;
